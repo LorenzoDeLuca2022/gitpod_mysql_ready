2a18bce4275a3f1a7da935571ffc5047ff87b3cf
LorenzoDeLuca2022 <deluca.lorenzo@iisgalvanimi.edu.it>
2023-02-12 13:58:42 +0000

    esercizio1

---
+++ main_1.py
@@ -0,0 +1,34 @@
+#creare un database in mysql
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
+
+#mycursor = mydb.cursor()
+
+#mycursor.execute("CREATE DATABASE mydatabase2")
+
+
+
+#ciclo for per leggere ogni riga di output del database (elenco database disponibili)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
+
+
+#permette di connettersi al database (se non mosta nulla è giusto, perchè non da errore)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="mydatabase2"
+)
